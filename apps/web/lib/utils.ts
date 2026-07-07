d4c19274427d0ee0828e82e3fdea0e5a145896e2
mrabukar <abukarmacalin08@gmail.com>
2026-07-07 22:45:51 +0300

    Add humanize() string helper to lib/utils.

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function humanize(value: string): string {
+  return value
+    .split("_")
+    .map((w) => w[0].toUpperCase() + w.slice(1))
+    .join(" ")
+}
