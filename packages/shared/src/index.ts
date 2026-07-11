aeadd9ca0302249ebcdd7e471f681b50a20de161
mrabukar <abukarmacalin08@gmail.com>
2026-07-11 17:29:09 +0300

    Export partner and news schemas from shared package

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./contact.schema";
 export * from "./category.schema";
 export * from "./product.schema";
+export * from "./partner.schema";
+export * from "./news.schema";
 export * from "./pagination.schema";
 export * from "./slugify";
